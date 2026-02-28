da28ad277995f9d74bc404d08636f2e5f4786ace
Md Nishan Chowdhury <mdnishanchowdhury.cse@gmail.com>
2026-02-28 21:07:49 +0600

    Add Banner and Categories

--- src/page/Home/Home.jsx
+++ src/page/Home/Home.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import OrganicHeroSection from '../../components/Home/OrganicHeroSection'
 
 function Home() {
   return (
     <div className='max-w-[1440px] mx-auto'>
+      <OrganicHeroSection />
       <h1>s</h1>
       <h1>s</h1>
       <h1>s</h1>
